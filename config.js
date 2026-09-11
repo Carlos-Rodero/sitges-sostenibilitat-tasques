7c4ea6f00e39244b3467797e7bfeeb16965571ba
Carlos Rodero - Sitges Film Festival <ecomanager@sitgesfilmfestival.com>
2026-09-11 14:30:19 +0200

    Connect Supabase

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Copia aquest fitxer com a config.js i introdueix les dades del teu projecte Supabase.
 // IMPORTANT: la clau anon/publishable és pública per disseny. La seguretat real es controla amb RLS a Supabase.
 window.APP_CONFIG = {
-  SUPABASE_URL: "PASTE_SUPABASE_URL_HERE",
-  SUPABASE_ANON_KEY: "PASTE_SUPABASE_ANON_KEY_HERE"
+  SUPABASE_URL: "https://zancifhsrxlvyixsbpod.supabase.co/rest/v1/",
+  SUPABASE_ANON_KEY: "sb_publishable_Q2nNHlcsTzDJZZ4Umrgj7w_4lYZSODs"
 };
